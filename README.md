6c986e41e65cf6da1797e08293eeace87bb77d2f
YingLi Lu <yinglilu@gmail.com>
2016-04-28 15:04:22 -0400

    1st stable version

---
+++ README.md
@@ -0,0 +1,21 @@
+Purpose: print meta-data of the mha/mhd/nrrd/nii/hdr file
+
+There are 4 options to see the meta-data:
+1. use notepad/notepad++ to open a mha file.
+   drawback: slow and may cause notepad to dead if load large mha file.
+2. use itk-snap to open it and check the information section.
+   drawback: slow, many button clicks.
+3. use c3d(http://www.itksnap.org/pmwiki/pmwiki.php?n=Convert3D.Documentation)
+   c3d mha_file -info-full
+4. use this tool: printh.exe
+   drawback: no:-). It is super fast, no itk dlls dependency.
+
+Usage: printh filename
+support format: mha,mhd,nrrd,nii,hdr 
+
+compile: 
+   windows or Linux:	
+		gcc -o printh printh.c nifti1_io.c znzlib.c -lm
+		note: for windows, need MinGW installed.
+   
+   or use cMakeLists.txt if you know cmake.
---
+++ printh.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <string.h>
+#include "nifti1_io.h"
+
+#define MAX_CHAR_EACH_LINE 1024
+
+int main(int argc, char*argv[])
+{
+	char filename[1024]={0};
+	char ext[5]={0};
+	char line[MAX_CHAR_EACH_LINE];
+	FILE *fp = NULL;
+	int num_of_lines_to_read = 0;
+	int n = 0;
+	nifti_1_header *nh = NULL;		
+
+	/*print usage*/
+	if (argc < 2)
+	{
+		printf("Usage: printheader filename\n");
+		printf("       support format: mha,mhd,nii,nrrd,and hdr.\n");
+		return -1;
+	}
+
+	/*open file*/
+	strcpy(filename,argv[1]);
+	fp=fopen(filename,"r");
+	if(!fp)
+	{
+		printf("Open file error!");
+		return -1;
+	}
+
+	/*get extension name*/
+	strncpy(ext,&filename[strlen(filename)-4],4);
+
+	/*number of lines to read
+		mha:11
+		mhd:10
+		nrrd:13
+	*/
+	if (strcmp(ext,".mha") == 0)
+	{
+		num_of_lines_to_read = 14;
+	}
+	else if (strcmp(ext,".mhd") == 0)
+	{
+		num_of_lines_to_read = 10;
+	}
+	else if (strcmp(ext,"nrrd") == 0)
+	{
+		num_of_lines_to_read = 13;
+	}
+	else if (strcmp(ext,".nii") == 0 || strcmp(ext,".hdr") == 0)
+	{
+		nh = nifti_read_header(filename, NULL, 1);
+		disp_nifti_1_header( "", nh );
+		return 0;
+	}
+	else
+	{
+		printf("unknown format! Support mha,mhd,nii,nrrd,and hdr only! \n");
+		return -1;
+	}
+
+	//read first n lines and print it
+	while( fgets(line, MAX_CHAR_EACH_LINE, fp) != NULL && n < num_of_lines_to_read) 
+	{
+		printf(line);
+		n++;
+	}
+
+	fclose(fp);
+
+	return 0;
+}
+
+
+
